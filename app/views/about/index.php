ef6df42b36e925a64286e67898c395c2681332e1
RizkiTrisna <rzkid.ra@gmail.com>
2020-05-13 16:46:38 +0700

    Update access token place

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,8 +1,8 @@
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h2 class="display-4">Hai, ini adalah <img src="<?= BASEURL; ?>/img/icon.png" alt="Logo PetaKurir.com" style="padding-bottom: 7px" height="70px" ></h2>
-        <p class="lead" style=" margin-top:20px;"><img src="<?= BASEURL; ?>/img/icon.png" alt="Logo PetaKurir.com" style="padding-bottom: 6px;" height="35px" > merupakan sistem informasi geografis berbasis web untuk menampilkan dan menambahkan data 
+        <h2 class="display-4">Hai, ini adalah <img src="<?= BASEURL; ?>/img/icon.png" alt="Logo PetaKurir.com" style="padding-bottom: 7px" height="70px"></h2>
+        <p class="lead" style=" margin-top:20px;"><img src="<?= BASEURL; ?>/img/icon.png" alt="Logo PetaKurir.com" style="padding-bottom: 6px;" height="35px"> merupakan sistem informasi geografis berbasis web untuk menampilkan dan menambahkan data
             alamat yang tidak tersedia di maps online konvensional. Sistem ini dibuat agar dapat membantu
-             mempercepat kurir paket dalam proses pencarian alamat yang tidak terdaftar di maps konvensional.</p>
+            mempercepat kurir paket dalam proses pencarian alamat yang tidak terdaftar di maps konvensional.</p>
     </div>
 </div>
